8d9d9d8b05db0744e219fe39770601f15430720b
Hernando17 <ginkotsu17@gmail.com>
2024-05-12 00:09:46 +0700

    template

--- warehouse_django/polls/migrations/0001_initial.py
+++ warehouse_django/polls/migrations/0001_initial.py
@@ -15,7 +15,9 @@ class Migration(migrations.Migration):
             name='Products',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nama', models.CharField(blank=True, max_length=100, null=True)),
+                ('name', models.CharField(blank=True, max_length=100, null=True)),
+                ('price', models.DecimalField(blank=True, decimal_places=2, max_digits=10, null=True)),
+                ('description', models.TextField(blank=True, null=True)),
             ],
             options={
                 'db_table': 'products',
--- warehouse_django/polls/models.py
+++ warehouse_django/polls/models.py
@@ -1,8 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Brands(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
 class Products(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    brand_id = models.ForeignKey(Brands, on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
 
-    class Meta:
-        db_table = 'products'
+class Locations(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+class StockQuantity(models.Model):
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE, blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    location_id = models.ForeignKey('Locations', on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+class StockMoves(models.Model):
+    product_id = models.ForeignKey(Products, on_delete=models.CASCADE, blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    location_id = models.ForeignKey(Locations, related_name='location_id', on_delete=models.CASCADE, blank=True, null=True)
+    location_dest_id = models.ForeignKey(Locations, related_name='location_dest_id',on_delete=models.CASCADE, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+
